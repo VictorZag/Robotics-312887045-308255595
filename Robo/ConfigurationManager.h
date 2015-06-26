45d03d9631a80e512683e1ed2695d00b27114c8f
Student Name <student@gmail.com>
2015-06-26 10:35:38 -0700

    copy parameters to configurationmanager

--- Robo/ConfigurationManager.h
+++ Robo/ConfigurationManager.h
@@ -7,13 +7,49 @@
 
 #ifndef CONFIGURATIONMANAGER_H
 #define	CONFIGURATIONMANAGER_H
+#include <stdlib.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
 
 class ConfigurationManager {
 public:
-    ConfigurationManager();
+    ConfigurationManager(char* p_filePath);
     ConfigurationManager(const ConfigurationManager& orig);
     virtual ~ConfigurationManager();
+    
+    string GetMapFilePath();
+    float GetStartLocationX();
+    float GetStartLocationY();
+    float GetStartAngle();
+    float GetGoalLocationX();
+    float GetGoalLocationY();
+    float GetRobotSize();
+    float GetMapResolutionCM();
+    float GetGridResolutionCM();
 private:
+    
+    string _mapFile;
+    float _startLocationX, _startLocationY, _startAngle;
+    float _goalLocationX, _goalLocationY;
+    float _robotSize,_mapResolutionCM,_gridResolutionCM;
+    
+        // Value-Defintions of the different String values
+    enum StringValue { map,
+                        startLocation,
+                        goal,
+                        robotSize,
+                        MapResolutionCM,
+                        GridResolutionCM };
+                          
+    // Map to associate the strings with the enum values
+    std::map<std::string, StringValue> s_mapStringValues;
 
 };
 
