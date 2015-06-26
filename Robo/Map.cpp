3945ac8cd7855b2c48e6a879fd24248ab0933f5e
Student Name <student@gmail.com>
2015-06-26 06:16:57 -0700

    Fix map.cpp

--- Robo/Map.cpp
+++ Robo/Map.cpp
@@ -9,36 +9,40 @@
 
 Map::Map(char* parameters) {
     ifstream myfile(parameters);
-    string line;
+    string line, propery;
+        int startPos = 0;                
+    int spacePos = 0;
     
     while (getline(myfile, line))
     {
         unsigned pos = line.find(':');
         if (pos != string::npos)
         {
-            switch (line.substr(0, pos - 1))
+            propery = line.substr(0, pos - 1);
+            switch (s_mapStringValues[propery])
             {
-                case 'map':
+                case (map):
                     _mapFile = line.substr( pos + 1);
                     break;
                     
-                case 'robotSize' :
-                    unsigned startPos = 0;                
-                    unsigned spacePos = 0;
+                case (robotSize):
+                    startPos = 0;      
+                    spacePos = 0;
                     while (spacePos = 0)
                         startPos = spacePos + 1;
                         spacePos = line.find(' ');
-                    _robotSize = max((float)atof(line.substr(startPos, spacePos - 1)), (float)atof(line.substr( spacePos + 1)));
+                    _robotSize = max((float)(atof(line.substr(startPos, spacePos - 1).c_str())),
+                                        (float)atof(line.substr( spacePos + 1).c_str())) ;
+                    break;
+                case (MapResolutionCM):
+                    _mapResolutionCM = (float)(atof(line.substr( pos + 1).c_str()));
                     break;
                     
-                case 'MapResolutionCM':
-                    _mapResolutionCM = (float)atof(line.substr( pos + 1));
+                case (GridResolutionCM):
+                    _gridResolutionCM = (float)(atof(line.substr( pos + 1).c_str()));
                     break;
-                    
-                case 'GridResolutionCM':
-                    _gridResolutionCM = (float)atof(line.substr( pos + 1));
+                default:
                     break;
-                    
                     
             }
         }
@@ -46,6 +50,12 @@ Map::Map(char* parameters) {
 }
 
 Map::Map(const Map& orig) {
+    s_mapStringValues["map"] = map;
+    s_mapStringValues["startLocation"] = startLocation;
+    s_mapStringValues["goal"] = goal;
+    s_mapStringValues["robotSize"] = robotSize;
+    s_mapStringValues["MapResolutionCM"] = MapResolutionCM;
+    s_mapStringValues["GridResolutionCM"] = GridResolutionCM;
 }
 
 Map::~Map() {
--- Robo/Map.h
+++ Robo/Map.h
@@ -7,22 +7,38 @@
 
 #ifndef MAP_H
 #define	MAP_H
+#include <stdlib.h>
 #include <vector>
+#include<fstream>
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 class Map {
 private:
-    char* _mapFile;
-    unsigned float _robotSize,_mapResolutionCM,_gridResolutionCM;
+    std::string _mapFile;
+    float _robotSize,_mapResolutionCM,_gridResolutionCM;
     unsigned _width, _height;
     vector<unsigned char> _image;
     vector< vector<int> > _grid;
+    
+    // Value-Defintions of the different String values
+    enum StringValue { map,
+                        startLocation,
+                        goal,
+                        robotSize,
+                        MapResolutionCM,
+                        GridResolutionCM };
+                          
+    // Map to associate the strings with the enum values
+    static std::map<std::string, StringValue> s_mapStringValues;
+    
 public:
     Map(char* parameters);
     Map(const Map& orig);
+    unsigned int str2int(const char* str, int h = 0);
     virtual ~Map();
 
 };
